22c517c7da8d35119cf6a284c7ee2e4fa29b52cd
aimalkhanofficial <aimalkhanofficial@gmail.com>
2021-03-28 19:17:37 -0400

    Adding support for upper and lower case letter detection

--- StrongPasswordChecker.js
+++ StrongPasswordChecker.js
@@ -1,7 +1,21 @@
+let stepCount = 0;
+
+const lengthInRange = (password) => password.length >= 6 && password.length <= 20;  
+const hasUpperCaseLetter = (password) => password.toLowerCase() != password;
+const hasLowerCaseLetter = (password) => password.toUpperCase() != password;
+
 /**
- * @param {string} password
+ * @param {string}ss password
  * @return {number}
  */
  var checkPassWord = function(password) {
-    
+    stepCount = 0;
+    if (!lengthInRange (password)) stepCount++;
+    if (!hasUpperCaseLetter(password)) stepCount++;
+    if (!hasLowerCaseLetter(password)) stepCount++;
+    return stepCount;
 };
+
+console.log('>>> Should return 5. Actual:', checkPassWord('a'));
+console.log('>>> Should return 3. Actual:', checkPassWord('aA1'));
+console.log('>>> Should return 0. Actual:', checkPassWord('1337C0d3'));
