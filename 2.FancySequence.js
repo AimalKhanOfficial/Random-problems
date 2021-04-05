fc7093395ae6e685e97d3f78d44d1e93034dabf6
aimalkhanofficial <aimalkhanofficial@gmail.com>
2021-04-04 21:43:45 -0400

    Passing 64/107 test cases

--- 2.FancySequence.js
+++ 2.FancySequence.js
@@ -1,5 +1,3 @@
-//Fancy Class
-
 var Fancy = function() {
     this.numbersList = [];
 }
@@ -21,11 +19,8 @@ Fancy.prototype.multAll = function(val) {
 }
 
 Fancy.prototype.getIndex = function(val) {
-    console.log('>> Gets the current value at index idx (0-indexed) of the sequence modulo 109 + 7. If the index is greater or equal than the length of the sequence, return -1.');
-}
-
-Fancy.prototype.showAll = function() {
-    console.log('>> Current', this.numbersList);
+    if (!this.numbersList[val]) return -1;
+    return this.numbersList[val];
 }
 
 let fancy = new Fancy();
@@ -40,4 +35,20 @@ fancy.multAll(2);  // fancy sequence: [13*2, 17*2, 10*2] -> [26, 34, 20]
 fancy.getIndex(0); // return 26
 fancy.getIndex(1); // return 34
 fancy.getIndex(2); // return 20
-fancy.showAll();
+
+
+//Failing at this use case (64/107 passed):
+/*
+["Fancy","append","append","getIndex","append","getIndex","addAll","append","getIndex","getIndex","append","append","getIndex","append","getIndex","append","getIndex","append","getIndex","multAll","addAll","getIndex","append","addAll","getIndex","multAll","getIndex","multAll","addAll","addAll","append","multAll","append","append","append","multAll","getIndex","multAll","multAll","multAll","getIndex","addAll","append","multAll","addAll","addAll","multAll","addAll","addAll","append","append","getIndex"]
+[[],[12],[8],[1],[12],[0],[12],[8],[2],[2],[4],[13],[4],[12],[6],[11],[1],[10],[2],[3],[1],[6],[14],[5],[6],[12],[3],[12],[15],[6],[7],[8],[13],[15],[15],[10],[9],[12],[12],[9],[9],[9],[9],[4],[8],[11],[15],[9],[1],[4],[10],[9]]
+*/
+
+/*
+Expected: 
+[null,null,null,8,null,12,null,null,24,24,null,null,4,null,12,null,20,null,24,null,null,37,null,null,42,null,360,null,null,null,null,null,null,null,null,null,220560,null,null,null,285845760,null,null,null,null,null,null,null,null,null,null,150746316]
+*/
+
+/*
+Mine: 
+[null,null,null,8,null,12,null,null,24,24,null,null,4,null,12,null,20,null,24,null,null,37,null,null,42,null,360,null,null,null,null,null,null,null,null,null,220560,null,null,null,285845760,null,null,null,null,null,null,null,null,null,null,17150746435]
+*/
